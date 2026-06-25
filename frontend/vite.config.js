3a9d3ff4f596f093714364d38ed2aeb192fe2de1
aidennh1 <aiden.nhowgate@gmail.com>
2026-06-25 13:32:00 -0400

    config to railway

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,9 +3,15 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+
   server: {
+    allowedHosts: ['osu-parking.up.railway.app'],
     proxy: {
       '/api': 'http://localhost:8080'
     }
+  },
+
+  preview: {
+    allowedHosts: ['osu-parking.up.railway.app']
   }
 })
